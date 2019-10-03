9dd2f6bee3fb18cbf043b9fd500a709d58ba1cb4
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-10-03 14:09:00 +0700

    Menambahkan file .htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,17 @@
 
 class App{
 	public function __construct(){
-		echo "OK";
+		// var_dump($_GET);
+		$url = $this->parseURL();
+		var_dump($url);
+	}
+
+	public function parseURL(){
+
+		if (isset($_GET['url'])){
+		$url = $_GET['url'];
+		return $url;
+		}
 	}
 
  }
