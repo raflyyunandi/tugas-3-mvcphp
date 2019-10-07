b3a561175c4d6bed11a038440c2752b4ac2adb2b
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-10-07 17:08:46 +0700

    Menambahkan Button untuk Insert Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,8 +1,7 @@
 <div class="Container mt-5">
 	<div class="row">
 		<div class="col-6">
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
+			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">Tambah data Mahasiswa
 </button>
 			<h3>Daftar Mahasiswa</h3>
 			<?php foreach ($data['mhs'] as $key ) : ?>
@@ -17,26 +16,50 @@
 </div> 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
 
+	      <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="">
+			  
+			  <div class="form-group">
+			    <label for="nama">Nama</label>
+			    <input type="text" class="form-control" id="nama" name="nama" laceholder="masukan nama">
+			  </div>
+
+	        	<div class="form-group">
+			    <label for="nrp">Nrp</label>
+			    <input type="number" class="form-control" id="nrp" name="nrp" laceholder="masukan nrp">
+			  </div>
+
+	        <div class="form-group">
+			    <label for="email">Email</label>
+			    <input type="email" class="form-control" id="email" name="email" laceholder="masukan email">
+			  </div>
+
+			  
+	        <div class="form-group">
+		    <label for="jurusan">Jurusan</label>
+		    <select class="form-control" id="jurusan">
+		      <option value="Teknik Informatika">Teknik Informatika</option>
+		      <option value="Teknik Mesin">Teknik Mesin</option>
+		      <option value="Teknik Pangan">Teknik Pangan</option>
+		      <option value="Teknik Planologi">Teknik Planologi</option>
+		      <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+		    </select>
+			</div>
+
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Tambah Data</button>
+	       	</form>
 <!-- <div class="Container mt-5">
 	<div class="row">
 		<div class="col-6">
