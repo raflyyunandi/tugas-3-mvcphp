bbc7f7e09ee166b203c5971405626ec159d28bd7
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-10-07 14:14:41 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,18 @@
+<?php 
+
+class About {
+	//public function page() {
+		//echo "About/page";
+	//public function index($nama, $pekerjaan) {
+	public function index($nama = 'Rafly', $pekerjaan = 'Gamer') {
+		echo "Halo nama saya $nama, saya adalah $pekerjaan";
+	}
+
+	public function page() {
+		echo "About/page";
+	}
+	
+}
+
+
+ ?>
