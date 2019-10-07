8a27c89d70a910c9b22bad904f3fd92b6aea4c17
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-10-07 14:39:54 +0700

    Menambahkan View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,15 +1,30 @@
 <?php 
 
-class About {
+class About extends Controller{
 	//public function page() {
 		//echo "About/page";
 	//public function index($nama, $pekerjaan) {
-	public function index($nama = 'Rafly', $pekerjaan = 'Gamer') {
-		echo "Halo nama saya $nama, saya adalah $pekerjaan";
+	public function index($nama = 'Rafly', $pekerjaan = 'Gamer',$umur = 32) {
+		//echo "Halo nama saya $nama, saya adalah $pekerjaan, saya berumur $umur tahun. ";
+		$data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view ('templates/header', $data);
+
+		$this->view ('about/index', $data);
+		
+		$this->view ('templates/footer');
 	}
 
 	public function page() {
-		echo "About/page";
+		//echo "About/page";
+		$data['judul'] = 'My Page';
+		$this->view ('templates/header', $data);
+
+		$this->view ('about/page');
+
+		$this->view ('templates/footer');
 	}
 	
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+	<h1>About Me</h1>
+	<p>Halo, nama saya <?=$data['nama'];?>, Umur Saya <?= $data['umur'];?> tahun, saya adalah seorang <?= $data['pekerjaan'];?></p>
