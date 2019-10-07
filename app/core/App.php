910af4b1ae91e99234451c79475e472489479c0e
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-10-07 10:28:25 +0700

    Menambuat atribut pada file App.php kemudian mengubah fungsi paseURL sedikit"
    ;

--- app/core/App.php
+++ app/core/App.php
@@ -1,17 +1,28 @@
 <?php 
 
 class App{
+	protected $controller = 'Home';
+	protected $method = 'index';
+	protected $params = [];
+
 	public function __construct(){
 		// var_dump($_GET);
 		$url = $this->parseURL();
-		var_dump($url);
+		// var_dump($url);
+		if (file_exists('../app/controller/' . $url[0] . '.php')) {
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
 	}
+
+	require_once '../app/controller' . $this->controller . '.php';
+	$this->controller = new $this->controller;
 
 	public function parseURL(){
 
 		if (isset($_GET['url'])){
-		$url = $_GET['url'];
-		$url = filter_var($url,FILTER_SANITAZE_URL);
+		$url = rtrim($_GET['url'], '/');
+		$url = filter_var($url, FILTER_SANITIZE_URL);
 		$url = explode('/', $url);
 		return $url;
 		}
