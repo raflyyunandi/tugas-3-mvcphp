17f96ae5e0f3ef7c1e9e7917d2414b6b618c04b5
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-10-07 10:40:26 +0700

    Membenarkan codingan sebelumnya pada App.php

--- app/core/App.php
+++ app/core/App.php
@@ -9,14 +9,29 @@ class App{
 		// var_dump($_GET);
 		$url = $this->parseURL();
 		// var_dump($url);
-		if (file_exists('../app/controller/' . $url[0] . '.php')) {
+		if (file_exists('../app/controllers/' . $url[0] . '.php')) {
 			$this->controller = $url[0];
 			unset($url[0]);
 		}
+
+	require_once '../app/controllers/' . $this->controller . '.php';
+	$this->controller = new $this->controller;
+ 
+	// method
+	if (isset($url[1])){
+		if (method_exists($this->cont, $url[1])){
+			$this->method = $url[1];
+			unset($url[1]);
+		}
 	}
 
-	require_once '../app/controller' . $this->controller . '.php';
-	$this->controller = new $this->controller;
+	//params
+	if (!empty($url)) {
+		var_dump($url);
+	}
+}
+
+	
 
 	public function parseURL(){
 
